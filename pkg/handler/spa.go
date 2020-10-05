52064572cbfbed8d58c84d1a1365ade00bf1d149
daniel.mueller <daniel.mueller@insitu.de>
2020-10-05 23:29:42 +0200

    fix spa handler to support history mode in vue

--- pkg/handler/spa.go
+++ pkg/handler/spa.go
@@ -32,7 +32,7 @@ func (h SpaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	_, err = os.Stat(path)
 	if os.IsNotExist(err) {
 		// file does not exist, serve index.html
-		http.ServeFile(w, r, filepath.Join(h.StaticPath, h.IndexPath))
+		http.ServeFile(w, r, h.IndexPath)
 		return
 	} else if err != nil {
 		// if we got an error (that wasn't that the file doesn't exist) stating the
