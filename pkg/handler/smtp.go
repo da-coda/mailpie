2e4a6dbccad798a29ad152a796dd3c36ae1afe10
daniel.mueller <daniel.mueller@insitu.de>
2021-01-19 18:36:17 +0100

    only log if there is a error

--- pkg/handler/smtp.go
+++ pkg/handler/smtp.go
@@ -8,5 +8,7 @@ import (
 
 var SmtpHandler = func(remoteAddr net.Addr, from string, to []string, data []byte) {
 	_, err := store.AddMail(data)
-	logrus.WithError(err).Error("Unable to add mail to store in SMTP handler")
+	if err != nil {
+		logrus.WithError(err).Error("Unable to add mail to store in SMTP handler")
+	}
 }
