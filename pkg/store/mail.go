093bb07eb924792a4c2112c67752f3a7982565ec
daniel.mueller <daniel.mueller@insitu.de>
2021-01-27 21:55:49 +0100

    change mailstore to use messagequeue interfaces instead, also remove left over code

--- pkg/store/mail.go
+++ pkg/store/mail.go
@@ -10,19 +10,14 @@ const EventDispatcher = "MailStore"
 
 type MailStore struct {
 	mails        map[string]instances.Mail
-	messageQueue event.MessageQueue
+	messageQueue event.Dispatcher
 }
 
-var mailStore *MailStore
-
-func CreateMailStore(messageQueue event.MessageQueue) *MailStore {
+func CreateMailStore(messageQueue event.Dispatcher) *MailStore {
 	var store *MailStore
 	store = &MailStore{messageQueue: messageQueue}
 	store.mails = make(map[string]instances.Mail)
-	if mailStore == nil {
-		mailStore = store
-	}
-	return mailStore
+	return store
 }
 
 func (store *MailStore) Add(key string, mailData instances.Mail) error {
@@ -38,10 +33,6 @@ func (store *MailStore) Set(key string, data instances.Mail) error {
 	store.mails[key] = data
 	store.messageQueue.Dispatch(NewMailStoredEvent, EventDispatcher, data)
 	return nil
-}
-
-func (store *MailStore) Get() (map[string]instances.Mail, error) {
-	return store.mails, nil
 }
 
 func (store *MailStore) GetSingle(key string) (instances.Mail, error) {
