f93f32bf26ec327029c953c332f4663785dcea0e
daniel.mueller <daniel.mueller@insitu.de>
2021-01-28 17:36:33 +0100

    add logo to landingpage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,6 @@
 <template>
   <v-layout>
-    <v-col cols="3" id="mailList">
-      <v-list>
-        <v-list-item v-for="mail in mails"></v-list-item>
-      </v-list>
-    </v-col>
-    <v-col cols="9" id="content">
-
-    </v-col>
+   <v-img src="@/assets/img/logo.png" max-width="400px" max-height="400px"/>
   </v-layout>
 </template>
 
@@ -26,15 +19,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-#mailList {
-  height: 100%;
-  background: blue;
-}
-
-#content {
-  height: 100%;
-  background: red;
-}
-</style>
